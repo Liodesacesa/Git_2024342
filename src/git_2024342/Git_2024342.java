ff1b7aca679b1dc18fde67dfa141af427a917c62
Liodesa <gabriel.chacon1111@gmail.com>
2025-01-26 00:12:53 +0000

    Added previous homework on strings

--- src/git_2024342/Git_2024342.java
+++ src/git_2024342/Git_2024342.java
@@ -4,6 +4,8 @@
  */
 package git_2024342;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ROG STRIX
@@ -18,6 +20,41 @@ public class Git_2024342 {
         
         System.out.println("Gabriel - 2024342");
         
+        // create a scanner that uses your keyboard
+        Scanner myKB = new Scanner(System.in);
+        
+        //initialize a string to holde the users answer
+        String userName;
+        
+        //prompt the user to enter their name for the first time
+        System.out.println("Please enter your name");
+        
+        //allows the user to input an answer
+        userName = myKB.nextLine().trim();
+        
+        /*
+        If the answer given contains any character not allowed
+        enters the user into a loop that repeatedly asks
+        for a new answer until the users answer only has letters
+        */
+        while (!userName.matches("[a-zA-Z!.?', ]+")){
+            System.out.println("No numbers please");
+            System.out.println("Now try entering your name again");
+            
+            userName = myKB.nextLine().trim();
+        }
+        
+        //checks the lenght of the answer given
+        int nameLenght = userName.length();
+        
+        //creates to strings with both halves of the answer given
+        String halfNameOne = userName.substring(0, nameLenght/2);
+        String halfNameTwo = userName.substring(nameLenght/2);
+        
+        //outputs the users answer turned around
+        System.out.println(halfNameTwo + halfNameOne);
+        
+  
     }
     
 }
